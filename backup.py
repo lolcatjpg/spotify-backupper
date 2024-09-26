ed366fbd601b2e4bcf6e15e7f215f23ac04efbe8
lolcatproductions <66800621+lolcatproductions@users.noreply.github.com>
2024-09-26 22:17:25 +0200

    backupper now reads ids from json file

--- backup.py
+++ backup.py
@@ -11,22 +11,26 @@ SECRET = os.getenv("SECRET")
 
 
 if __name__ == "__main__":
+    with open("playlist_ids.json", "r", encoding="utf-8") as f:
+        playlist_ids = json.loads(f.read())
+
     client_credentials_manager = SpotifyClientCredentials(client_id=CID, client_secret=SECRET)
     sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
-    playlist_name = sp.playlist(
-        "spotify:playlist:6SvaET2rqGHOtxeh6RwrT2", 
-        fields="name"
-    )["name"]
+    for playlist_id in playlist_ids:
+        playlist_name = sp.playlist(
+            playlist_id,
+            fields="name"
+        )["name"]
 
-    playlist = sp.playlist_items(
-        "spotify:playlist:6SvaET2rqGHOtxeh6RwrT2", 
-        fields="items.track.name, items.track.artists.name, items.track.album.name, items.track.id",
-    )
+        playlist = sp.playlist_items(
+            playlist_id,
+            fields="items.track.name, items.track.artists.name, items.track.album.name, items.track.id",
+        )
 
-    with open(
-        f"backup/playlist-backup-{datetime.now()}-{playlist_name}.json",
-        "x",
-        encoding="utf-8"
-    ) as f:
-        f.write(json.dumps(playlist))
+        with open(
+            f"backup/playlist-backup-{datetime.now()}-{playlist_name}.json",
+            "x",
+            encoding="utf-8"
+        ) as f:
+            f.write(json.dumps(playlist))
