9aa4376d9aeaffab25adb970e5505013d7db0d51
lolcatproductions <66800621+lolcatproductions@users.noreply.github.com>
2024-09-26 21:27:21 +0200

    finished csv converter

--- json_to_csv.py
+++ json_to_csv.py
@@ -8,13 +8,15 @@ if __name__ == "__main__":
         json_data = json.loads(f.read())
 
     with open("playlist.csv", "x", encoding="utf-8", newline="") as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerow(["title", "artist", "album", "id"])
+
         for track in json_data["items"]:
             csv_row = []
             csv_row.append(track["track"]["name"])
             artists = [artist["name"] for artist in track["track"]["artists"]]
             csv_row.append(", ".join(artists))
             csv_row.append(track["track"]["album"]["name"])
+            csv_row.append(track["track"]["id"])
 
-            csvwriter = csv.writer(csvfile)
-            csvwriter.writerow(["title", "artist", "album", "id"])
             csvwriter.writerow(csv_row)
