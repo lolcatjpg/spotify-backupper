634145a1eb1d147322ecfe137d40b10dc89a2f1f
lolcatproductions <66800621+lolcatproductions@users.noreply.github.com>
2024-09-26 22:08:53 +0200

    backup filename now includes playlist name

--- backup.py
+++ backup.py
@@ -14,12 +14,19 @@ if __name__ == "__main__":
     client_credentials_manager = SpotifyClientCredentials(client_id=CID, client_secret=SECRET)
     sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
+    playlist_name = sp.playlist(
+        "spotify:playlist:6SvaET2rqGHOtxeh6RwrT2", 
+        fields="name"
+    )["name"]
+
     playlist = sp.playlist_items(
         "spotify:playlist:6SvaET2rqGHOtxeh6RwrT2", 
         fields="items.track.name, items.track.artists.name, items.track.album.name, items.track.id",
-        additional_types=['track']
     )
-    print(playlist)
 
-    with open(f"backup/playlist-backup-{datetime.now()}.json", "x", encoding="utf-8") as f:
+    with open(
+        f"backup/playlist-backup-{datetime.now()}-{playlist_name}.json",
+        "x",
+        encoding="utf-8"
+    ) as f:
         f.write(json.dumps(playlist))
