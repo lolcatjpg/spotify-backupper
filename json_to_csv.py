f00300a7b209e7374e45c60c6ef3225fdfe73d56
lolcatproductions <66800621+lolcatproductions@users.noreply.github.com>
2024-09-26 21:23:39 +0200

    made csv converter

---
+++ json_to_csv.py
@@ -0,0 +1,20 @@
+import csv
+import json
+from sys import argv
+
+
+if __name__ == "__main__":
+    with open(argv[1], encoding="utf-8") as f:
+        json_data = json.loads(f.read())
+
+    with open("playlist.csv", "x", encoding="utf-8", newline="") as csvfile:
+        for track in json_data["items"]:
+            csv_row = []
+            csv_row.append(track["track"]["name"])
+            artists = [artist["name"] for artist in track["track"]["artists"]]
+            csv_row.append(", ".join(artists))
+            csv_row.append(track["track"]["album"]["name"])
+
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerow(["title", "artist", "album", "id"])
+            csvwriter.writerow(csv_row)
--- main.py
+++ main.py
@@ -6,19 +6,20 @@ from dotenv import load_dotenv
 from spotipy.oauth2 import SpotifyClientCredentials
 
 load_dotenv()
-
 CID = os.getenv("CID")
 SECRET = os.getenv("SECRET")
 
-client_credentials_manager = SpotifyClientCredentials(client_id=CID, client_secret=SECRET)
-sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
-playlist = sp.playlist_items(
-    "spotify:playlist:6SvaET2rqGHOtxeh6RwrT2", 
-    fields="items.track.name, items.track.artists.name, items.track.album.name, items.track.id",
-    additional_types=['track']
-)
-print(playlist)
+if __name__ == "__main__":
+    client_credentials_manager = SpotifyClientCredentials(client_id=CID, client_secret=SECRET)
+    sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
-with open(f"backup/playlist-backup-{datetime.now()}.json", "x", encoding="utf-8") as f:
-    f.write(json.dumps(playlist))
+    playlist = sp.playlist_items(
+        "spotify:playlist:6SvaET2rqGHOtxeh6RwrT2", 
+        fields="items.track.name, items.track.artists.name, items.track.album.name, items.track.id",
+        additional_types=['track']
+    )
+    print(playlist)
+
+    with open(f"backup/playlist-backup-{datetime.now()}.json", "x", encoding="utf-8") as f:
+        f.write(json.dumps(playlist))
